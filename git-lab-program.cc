28bc0f22c46d2147ef87cb05167709e7d6bd5ea6
JaredC98-OU <jc188820@ohio.edu>
2023-01-24 11:22:43 -0500

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+//**********************************
+//
+//  File    :       git-lab-program.cc
+//  Author  :       Jared Cominsky
+//  Date    :       24\1\2023
+//  Email   :       jc188820@ohio.edu
+//
+//  Description :   This program writes to an output file
+//
+//**********************************
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!!" << endl;
+    return(0);
+}//main
